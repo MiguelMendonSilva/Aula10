2eb58833eebf59926562d091ea37ee9eb9567947
Miguel <124186796+MiguelMendonSilva@users.noreply.github.com>
2025-04-27 18:28:58 -0300

    Create util.h

---
+++ Aula10/util.h
@@ -0,0 +1,17 @@
+double diametro(double raio){
+    double diametro;
+    diametro = raio * 2;
+    return diametro;
+}
+
+double circunferencia(double raio){
+    double circunferencia;
+    circunferencia = (3.1415 * 2) * raio;
+    return circunferencia;
+}
+
+double area(double raio){
+    double area;
+    area = 3.1415 * (raio * raio);
+    return area;
+}

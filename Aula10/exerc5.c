2bfb22d83a4f7ca703c83b98bf2caecb2a8f35ed
Miguel <124186796+MiguelMendonSilva@users.noreply.github.com>
2025-04-27 17:52:25 -0300

    Update exerc5.c

--- Aula10/exerc5.c
+++ Aula10/exerc5.c
@@ -1,16 +1,19 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 
-
-int soma(int a, int b){
+int soma(int a, int b) {
     return a + b;
-    
+}
+
+void imprimeSoma(int resultado) {
+    printf("O resultado é: %d\n", resultado);
 }
 
 int main() {
     int x = 5;
     int y = 5;
     int resultado = soma(x, y);
-    printf("O resultado é: %d", resultado);
+    
+    imprimeSoma(resultado); 
+    
     return 0;
 }
